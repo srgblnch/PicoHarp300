7de1e687b4252cbb777f5dafaa31925c2e77707f
sergiblanch <sergiblanch@3b39871e-fe4b-0410-b5db-e82997f68959>
2014-06-05 13:44:38 +0000

    add a super class to manage logging write. set up instrument class methods to wrap the c library calls into a pythonic way. complete the readme with the steps to do to repeate the dlldemo (example given by the manufacturer)
    git-svn-id: svn+ssh://svn.code.sf.net/p/tango-ds/code/DeviceClasses/MeasureInstruments/PicoHarp300/trunk@17409 3b39871e-fe4b-0410-b5db-e82997f68959

--- library/PicoHarp.pyx
+++ library/PicoHarp.pyx
@@ -31,10 +31,29 @@
 ###############################################################################
 
 from version import MAJOR_VERSION,MINOR_VERSION,BUILD_VERSION,REVISION_VERSION
+import time
+cimport cython
+from libc.stdlib cimport malloc, free
+#cimport numpy as np
+
+cdef extern from "phlib.h":
+    int PH_GetErrorString(char* errstring, int errcode)
+
+class Logger:
+    '''Very basic debugging flag mode
+    '''
+    def __init__(self,debug):
+        self.__debug = debug
+    def debug(self,msg):
+        if self.__debug:
+            print(msg)
+    def interpretError(self,err):
+        ErrorString = " "*40
+        PH_GetErrorString(ErrorString, err)
+        return ErrorString
 
 cdef extern from "phlib.h":
     int PH_GetLibraryVersion(char* version)
-    int PH_GetErrorString(char* errstring, int errcode)
 cdef extern from "errorcodes.h":
     int ERROR_NONE
     int ERROR_DEVICE_OPEN_FAIL
@@ -54,42 +73,52 @@ def __version__():
 
 cdef extern from "phlib.h":
     int PH_OpenDevice(int devidx, char* serial)
-    #int PH_CloseDevice(int devidx)
-    #int PH_Initialize(int devidx, int mode)
 cdef extern from "phdefin.h":
     int MAXDEVNUM
 
-class Discoverer:
-    def __init__(self):
+class Discoverer(Logger):
+    def __init__(self,debug=False):
+        Logger.__init__(self, debug)
         self._instruments = {}
         self.__getInstruments()
 
     def __getInstruments(self):
+        '''Iterate between the possible IDs, looking for plugged instruments.
+           If there is some, try to get the serial or in case of error, 
+           interpret the returned error.
+        '''
         sn = " "*10
-        print("Searching for PicoHarp devices...")
-        print("Devidx     Status")
+        self.debug("Searching for PicoHarp devices...")
+        self.debug("Devidx     Status")
         for i in range(MAXDEVNUM):
             err = PH_OpenDevice(i,sn)
             if err == ERROR_NONE:
                 code = strcut(sn)
-                print("  %1d        S/N %s"%(i,code))
+                self.debug("  %1d        S/N %s"%(i,code))
                 self._instruments[code] = i
             elif err == ERROR_DEVICE_OPEN_FAIL:
-                print("  %1d        no device"%(i))
+                self.debug("  %1d        no device"%(i))
             else:
-                Errorstring = " "*40
-                PH_GetErrorString(Errorstring, err)
-                print("  %1d        %s"%(i,Errorstring))
+                #Errorstring = " "*40
+                #PH_GetErrorString(Errorstring, err)
+                self.debug("  %1d        %s"%(i,self.interpretError(err)))
 
     @property
     def count(self):
+        '''Returns how many instruments have been found.
+        '''
         return len(self._instruments.keys())
 
     @property
     def serials(self):
+        '''List of all the serial numbers found
+        '''
         return self._instruments.keys()
 
     def search(self,serial):
+        '''Given a serial of an instrument, get its ID.
+           Or exception if it's not present
+        '''
         if serial in self._instruments.keys():
             return self._instruments[serial]
         else:
@@ -97,36 +126,307 @@ class Discoverer:
 
 cdef extern from "phlib.h":
     #int PH_OpenDevice(int devidx, char* serial)
-    #int PH_CloseDevice(int devidx)
+    int PH_CloseDevice(int devidx)
     int PH_Initialize(int devidx, int mode)
     int PH_GetHardwareInfo(int devidx, char* model, 
                            char* partno, char* version)
     int PH_Calibrate(int devidx)
+    int PH_SetSyncDiv(int devidx, int div)
+    int PH_SetInputCFD(int devidx, int channel, int level, int zc)
+    int PH_SetBinning(int devidx, int binning)
+    int PH_SetOffset(int devidx, int offset)
+    int PH_GetResolution(int devidx, double* resolution)
+    int PH_GetCountRate(int devidx, int channel, int* rate)
+    int PH_SetStopOverflow(int devidx, int stop_ovfl, int stopcount)
+    int PH_ClearHistMem(int devidx, int block)
+    int PH_StartMeas(int devidx, int tacq)
+    int PH_StopMeas(int devidx)
+    int PH_CTCStatus(int devidx, int* ctcstatus)
+    int PH_GetHistogram(int devidx, unsigned int* chcount, int block)
+    int PH_GetFlags(int devidx, int* flags)
 cdef extern from "phdefin.h":
     int MODE_HIST
     #int MODE_T2
     #int MODE_T3
+    int HISTCHAN
+    int FLAG_OVERFLOW
+    int ZCMIN
+    int ZCMAX
+    int DISCRMIN
+    int DISCRMAX
 
-class Instrument:
-    def __init__(self,devidx,mode=MODE_HIST):
+class Instrument(Logger):
+    def __init__(self,devidx,mode=MODE_HIST,divider=8,binning=0,offset=0,
+                 acqTime=1000,CFDLevels=[100,100],CFDZeroCross=[10,10],
+                 debug=False):
+        Logger.__init__(self, debug)
         self._devidx = devidx
+        if mode != MODE_HIST:
+            raise NotImplementedError("Only histogram mode supported")
         self._mode = mode
-        err = PH_Initialize(self._devidx,self._mode)
-        if err != ERROR_NONE:
-            raise IOError("Init error (%d)"%(err))
-        print("Instrument initialised")
+        self.initialise()
         self._HW_Model = " "*16
         self._HW_PartNo = " "*8
         self._HW_Version = " "*8
+        self.__getHardwareInfo()
+        self.__calibrate()
+        #configurable parameters
+        self._SyncDivider = divider
+        if CFDLevels[0] < DISCRMIN or CFDLevels[1] < DISCRMIN:
+            raise ValueError("CFD levels must be above %d"%(DISCRMIN))
+        if CFDLevels[0] > DISCRMAX or CFDLevels[1] > DISCRMAX:
+            raise ValueError("CFD levels must be below %d"%(DISCRMAX))
+        self._CFDLevel = CFDLevels
+        if CFDZeroCross[0] < ZCMIN or CFDZeroCross[1] < ZCMIN:
+            raise ValueError("CFD zero cross must be above %d"%(ZCMIN))
+        if CFDZeroCross[0] > ZCMAX or CFDZeroCross[1] > ZCMAX:
+            raise ValueError("CFD zero cross must be below %d"%(ZCMAX))
+        self._CFDZeroCross = CFDZeroCross
+        self._Binning = binning
+        self._Offset = offset
+        self._acquisitionTime = acqTime#ms
+        #readonly parameters
+        self._Resolution = 0.0
+        #outputs
+        self._CountRate = [0,0]
+        self._counts = [0]*HISTCHAN
+        self._flags = 0
+        self._integralCount = 0.0
+        self.prepare()
+
+    def prepare(self):
+        self.setSyncDivider()
+        self.setInputCFD(0)
+        self.setInputCFD(1)
+        self.setBinning()
+        self.setOffset()
+        self.getResolution()
+        self.getCountRates()
+        #From dlldemo.c: after Init or SetSyncDiv you must allow 100 ms 
+        #for valid new count rate readings.
+        time.sleep(0.2)#200ms
+        self.setStopOverflow()
+
+    def acquire(self):
+        self.clearHistMem()
+        self.getCountRates()
+        self.startMeas()
+        waitloop = 0
+        while(self.getCounterStatus==0):
+            waitloop += 1
+        self.stopMeas()
+        self.getHistogram()
+        self.getFlags()
+        self.integralCount()
+        self.debug("waitloop = %d total count = %d"
+                   %(waitloop,self._integralCount))
+        if (self._flags&FLAG_OVERFLOW):
+            self.debug("Overflow!")
+
+    def __del__(self):
+        err = PH_CloseDevice(self._devidx)
+        if err != ERROR_NONE:
+            raise IOError("Close error (%d)"%(err))
+        self.debug("Instrument connection closed.")
+
+    def initialise(self):
+        err = PH_Initialize(self._devidx,self._mode)
+        if err != ERROR_NONE:
+            raise IOError("Init error (%d): %s"%(err,self.interpretError(err)))
+        self.debug("Instrument initialised.")
+
+    def __getHardwareInfo(self):
         err = PH_GetHardwareInfo(self._devidx,self._HW_Model,
                                  self._HW_PartNo,self._HW_Version)
         if err != ERROR_NONE:
-            raise IOError("Getting hardware info error (%d)"%(err))
-        print("Found Model %s Partnum %s Version %s"%(self._HW_Model,
-                                                      self._HW_PartNo,
-                                                      self._HW_Version))
+            raise IOError("Getting hardware info error (%d): %s"
+                          %(err,self.interpretError(err)))
+        self.debug("Found Model %s Partnum %s Version %s"%(self._HW_Model,
+                                                           self._HW_PartNo,
+                                                           self._HW_Version))
+        self._HW_Model = strcut(self._HW_Model)
+        self._HW_PartNo = strcut(self._HW_PartNo)
+        self._HW_Version = strcut(self._HW_Version)
+    def __calibrate(self):
         err = PH_Calibrate(self._devidx)
         if err != ERROR_NONE:
-            raise IOError("Calibration error (%d)"%(err))
-        print("Instrument calibration done")
+            raise IOError("Calibration error (%d): %s"
+                          %(err,self.interpretError(err)))
+        self.debug("Instrument calibration done.")
+
+    @property
+    def __model__(self):
+        return self._HW_Model
+
+    @property
+    def __partnum__(self):
+        return self._HW_PartNo
+
+    @property
+    def __version__(self):
+        return self._HW_Version
+
+    def getSyncDivider(self):
+        return self._SyncDivider
+    def setSyncDivider(self,syncDivider=None):
+        if syncDivider == None:
+            syncDivider = self._SyncDivider
+        err = PH_SetSyncDiv(self._devidx,syncDivider)
+        if err != ERROR_NONE:
+            raise IOError("SetSyncDiv error (%d): %s"
+                          %(err,self.interpretError(err)))
+        self._SyncDivider = syncDivider
+        self.debug("SetSyncDiv has been set (%d)"%(self._SyncDivider))
     
+    #TODO: a getter
+    def setInputCFD(self,channel,CFDZeroCross=None,CFDLevel=None):
+        if CFDZeroCross == None:
+            CFDZeroCross = self._CFDZeroCross[channel]
+        if CFDLevel == None:
+            CFDLevel = self._CFDLevel[channel]
+        err = PH_SetInputCFD(self._devidx,channel,CFDLevel,CFDZeroCross)
+        if err != ERROR_NONE:
+            raise IOError("setInputCFD error (%d): %s"
+                          %(err,self.interpretError(err)))
+        self.debug("setInputCFD has been set for channel %d: "\
+                   "(CFDLevel:%d,CFDZeroCross:%d)"
+                   %(channel,CFDLevel,CFDZeroCross))
+        self._CFDZeroCross[channel] = CFDZeroCross
+        self._CFDLevel[channel] = CFDLevel
+
+    def getBinning(self):
+        return self._Binning
+    def setBinning(self,Binning=None):
+        if Binning == None:
+            Binning = self._Binning
+        err = PH_SetBinning(self._devidx,Binning)
+        if err != ERROR_NONE:
+            raise IOError("SetBinning error (%d): %s"
+                          %(err,self.interpretError(err)))
+        self._Binning = Binning
+        self.debug("Binning = %d"%(self._Binning))
+        
+    def getOffset(self):
+        return self._Offset
+    def setOffset(self,Offset=None):
+        if Offset == None:
+            Offset = self._Offset
+        err = PH_SetOffset(self._devidx,Offset)
+        if err != ERROR_NONE:
+            raise IOError("SetOffset error (%d): %s"
+                          %(err,self.interpretError(err)))
+        self._Offset = Offset
+        self.debug("Offset = %d"%(self._Offset))
+
+    def getResolution(self):
+        cdef double resolution = 0.0
+        err = PH_GetResolution(self._devidx,&resolution)
+        if err != ERROR_NONE:
+            raise IOError("GetResolution error (%d): %s"
+                          %(err,self.interpretError(err)))
+        self._Resolution = resolution
+        self.debug("Resolution = %g"%(self._Resolution))
+        return self._Resolution
+    #There is no setter
+    
+    def getCountRate(self,channel):
+        cdef int countrate = 0
+        err = PH_GetCountRate(self._devidx,channel,&countrate)
+        if err != ERROR_NONE:
+            raise IOError("GetCountRate channel %d error (%d): %s"
+                          %(channel,err,self.interpretError(err)))
+        self._CountRate[channel] = countrate
+        self.debug("CountRate[%d] = %d"%(channel,self._CountRate[channel]))
+        return countrate
+    def getCountRates(self,channel=None):
+        if channel == None:
+            channel = [0,1]
+        elif channel in [0,1]:
+            channel = [channel]
+        else:
+            raise IndexError("Channel not well specified.")
+        for i in channel:
+            self.getCountRate(i)
+        return self._CountRate
+    
+    def setStopOverflow(self):
+        err = PH_SetStopOverflow(self._devidx,1,HISTCHAN-1)
+        if err != ERROR_NONE:
+            raise IOError("SetStopOverflow error (%d): %s"
+                          %(err,self.interpretError(err)))
+        self.debug("Overflow stopper set")
+
+    def clearHistMem(self):
+        err = PH_ClearHistMem(self._devidx,0)
+        if err != ERROR_NONE:
+            raise IOError("ClearHistMem error (%d): %s"
+                          %(err,self.interpretError(err)))
+        self.debug("Histogram memory clean")
+
+    def getAcquisitionTime(self):
+        return self._acquisitionTime
+    def setAcquisitionTime(self,AcquisitionTime):
+        self._acquisitionTime = AcquisitionTime
+
+    def startMeas(self,AcquisitionTime=None):
+        if AcquisitionTime == None:
+            AcquisitionTime = self._acquisitionTime
+        err = PH_StartMeas(self._devidx,AcquisitionTime)
+        if err != ERROR_NONE:
+            raise IOError("StartMeas error (%d): %s"
+                          %(err,self.interpretError(err)))
+        self.debug("start measurement")
+    
+    def getCounterStatus(self):
+        cdef int ctcstatus=0
+        err = PH_CTCStatus(self._devidx,&ctcstatus)
+        if err != ERROR_NONE:
+            raise IOError("CTCStatus error (%d): %s"
+                          %(err,self.interpretError(err)))
+        self.debug("counter status = %d"%(ctcstatus))
+        return ctcstatus
+
+    def stopMeas(self):
+        err = PH_StopMeas(self._devidx)
+        if err != ERROR_NONE:
+            raise IOError("StopMeas error (%d): %s"
+                          %(err,self.interpretError(err)))
+        self.debug("stop measurement")
+
+    def getHistogram(self):
+        #cdef np.ndarray[np.uint32_t, ndim=1] counts = np.zeros([HISTCHAN],
+        #                                                      dtype=np.uint32)
+        cdef unsigned int *counts
+        counts = <unsigned int*>malloc(HISTCHAN*cython.sizeof(int))
+        err = PH_GetHistogram(self._devidx,counts,0)
+        if err != ERROR_NONE:
+            raise IOError("GetHistogram error (%d): %s"
+                          %(err,self.interpretError(err)))
+        for i in xrange(len(self._counts)):
+            self._counts[i] = counts[i]
+        free(counts)
+        if len(self._counts)>21:
+            self.debug("Histogram: %s (...) %s"
+                       %(repr(self._counts[:7])[:-1],
+                         repr(self._counts[-7:])[1:]))
+        else:
+            self.debug("Histogram: %s"%(self._counts))
+        return self._counts
+
+    def getFlags(self):
+        cdef int flags = 0
+        err = PH_GetFlags(self._devidx,&flags)
+        if err != ERROR_NONE:
+            raise IOError("GetFlags error (%d): %s"
+                          %(err,self.interpretError(err)))
+        self._flags = flags
+        self.debug("Flags: %s"%(bin(self._flags)))
+        return self._flags
+
+    def integralCount(self):
+        integralCount = 0
+        for count in self._counts:
+            integralCount += count
+        self._integralCount = integralCount
+        self.debug("Integral count = %d"%(self._integralCount))
+        return self._integralCount
